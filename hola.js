cc63897fec8a685662e500732c1ddf2a9ff8d929
Cam1Maturano <camila.maturano.kiltex@gmail.com>
2025-06-10 17:28:36 -0300

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,3 +1,4 @@
-const nombre = process.env.USERNAME || 'usuario desconocido';
+const nombre = process.env.USERNAME || 'Usuario';
+const lenguaje = process.env.LANGUAGE || 'lenguaje secreto';
 
-console.log(`¡Hola, ${nombre} desde GitHub!`);
+console.log(`¡Hola, ${nombre}! Tu lenguaje favorito es ${lenguaje}.`);
